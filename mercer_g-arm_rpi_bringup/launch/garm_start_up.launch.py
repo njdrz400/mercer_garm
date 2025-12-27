a7210c8cdc96c60090394917197fd66bc23811ba
mizereks <njdrz400@yahoo.com>
2025-12-27 13:12:50 -0500

    updated rpi launch

---
+++ mercer_g-arm_rpi_bringup/launch/garm_start_up.launch.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+import os
+from glob import glob
+
+from launch import LaunchDescription
+from launch.actions import TimerAction
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # --- Node: pi_gpio_server ---
+    gpio_node = Node(
+        package="pi_gpio",
+        executable="pi_gpio_server",
+        name="pi_gpio_server",
+        output="screen"
+    )
+
+    # --- Node: g_arm driver ---
+    garm_node = Node(
+        package="g_arm",
+        executable="driver",
+        name="g_arm_driver",
+        output="screen",
+        parameters=[
+            {"X_ZERO_REAL_ANGLE_DEFAULT": 115.0}
+        ],
+    )
+
+    #
+    # OPTIONAL: delay node start until system hardware/network is ready
+    # (equivalent to `sleep 5`)
+    #
+    delayed_start = TimerAction(
+        period=5.0,   # seconds
+        actions=[gpio_node, garm_node]
+    )
+
+    return LaunchDescription([
+        delayed_start
+    ])
+
--- mercer_g-arm_rpi_bringup/setup.py
+++ mercer_g-arm_rpi_bringup/setup.py
@@ -1,3 +1,7 @@
+
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'mercer_g-arm_rpi_bringup'
@@ -8,9 +12,12 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='scott',
