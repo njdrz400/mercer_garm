7f0f682f82e3a006b2a695360414fe657f6f802a
scott <mizereks@mccc.edu>
2025-12-27 16:30:04 -0500

    updated launch and safety proxy.py

--- mercer_g-arm_rpi_bringup/launch/garm_start_up.launch.py
+++ mercer_g-arm_rpi_bringup/launch/garm_start_up.launch.py
@@ -9,6 +9,23 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    package_path = get_package_share_path('g_arm_description')
+    default_model_path = os.path.join(package_path, 'urdf', 'robot_electromagnet.urdf.xacro')
+
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                                      description='Absolute path to robot urdf file')
+
+
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+                                       value_type=str)
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+
     # --- Node: pi_gpio_server ---
     gpio_node = Node(
         package="pi_gpio",
@@ -40,6 +57,9 @@ def generate_launch_description():
     return LaunchDescription([
         garm_node,
         gpio_node,
-        delayed_start
+        delayed_start,
+        model_arg,
+        joint_state_publisher_node,
+        robot_state_publisher_node,
     ])
 
